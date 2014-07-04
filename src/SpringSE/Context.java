30d27e0a5448a1ad7c12ebb493e01b896a2c382e
OpenNeptune <neptunepos@gmail.com>
2014-07-05 00:45:52 +0800

    springse

--- src/SpringSE/Context.java
+++ src/SpringSE/Context.java
@@ -1,24 +1,13 @@
-package spring.javaSe.helloword.test;
+package SpringSE;
 
-
-import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-import spring.javaSe.helloword.bean.HelloWorld;
-
-public class Test_HelloWorld {
-
-	@SuppressWarnings("resource")
-	@Test
-	public void say(){
+public class Context {
+	public static ApplicationContext getAppContext(){
 		//此时为了方便做单元测试，故使用FileSystemXmlApplicationContext-获取配置文件的路径
 		ApplicationContext ctx = new FileSystemXmlApplicationContext(
 				"WebContent/WEB-INF/conf/applicationContext-javasebeantest.xml");
-		
-		HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
-		
-		helloWorld.say();
-
+		return ctx;
 	}
 }
---
+++ src/SpringSE/TestScope/TestScope.java
@@ -0,0 +1,30 @@
+﻿package SpringSE.TestScope;
+
+import org.springframework.context.ApplicationContext;
+
+import SpringSE.Context;
+
+public class TestScope {
+	
+	public String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+	public static void main(String[] args){
+		ApplicationContext ctx = Context.getAppContext();
+		
+		for(int i=0;i<5;i++){
+			TestScope test = (TestScope) ctx.getBean("testScope");
+			
+			System.out.println(test);	
+		}
+
+		
+	}
+}
